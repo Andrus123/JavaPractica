c134fa43f30e9d81f44374fccf38f4c67724e13b
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-02 20:34:05 -0400

    Ejercicios de POO: Desarrollar un programa Banco con lectura de clientes

---
+++ src/PracticaPoliformismo/Biblioteca.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticaPoliformismo;
+
+/**
+ *
+ * @author andru
+ */
+public class Biblioteca {
+    private String nombre;
+    private int nrolibros;
+    private String[][] libro = new String[40][3];
+    
+}
