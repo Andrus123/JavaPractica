0f8adcc5fde910764332226db214919161535a7b
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-05 22:26:57 -0400

    Frase: quedó pinton el algoritmo que le metí.

--- src/PracticaPoliformismo/Frase.java
+++ src/PracticaPoliformismo/Frase.java
@@ -19,7 +19,7 @@ public class Frase {
         public Frase(){
             this.nropalabras = 25;
             this.titulo = "Ozymandias";
-            palabra[0] = "And";  palabra[13] = "kings:";  
+            palabra[0] = "And";  palabra[13] = "kings.";  
             palabra[1] = "on";  palabra[14] = "Look"; 
             palabra[2] = "the";  palabra[15] = "on"; 
             palabra[3] = "pedestal";  palabra[16] = "my"; 
@@ -30,21 +30,79 @@ public class Frase {
             palabra[8] = "name";  palabra[21] = "despair!’"; 
             palabra[9] = "is";  palabra[22] = "Nothing"; 
             palabra[10] = "Ozymandias"; palabra[23] = "beside"; 
-            palabra[11] = " king"; palabra[24] = "remains."; 
+            palabra[11] = "king"; palabra[24] = "remains."; 
             palabra[12] = "of";  
         }
         //Getters y Setters
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNropalabras() {
+        return nropalabras;
+    }
+
+    public void setNropalabras(int nropalabras) {
+        this.nropalabras = nropalabras;
+    }
+
+    public String[] getPalabra() {
+        return palabra;
+    }
+
+    public void setPalabra(String[] palabra) {
+        this.palabra = palabra;
+    }
         
-        public void leer(){
-            System.out.println("Titulo del Poema: ");
-            this.titulo = Leer.dato();
-            System.out.println("Cantidad de Palabras: ");
-            this.nropalabras = Leer.datoInt();
-            for(int i=0;i<nropalabras;i++){
-                if(palabra[i]==" "){
-                    palabra[i+1]=Leer.dato();
-                }
+    public void leer(){
+    System.out.println("Titulo del Poema: ");
+    this.titulo = Leer.dato();
+    System.out.println("Cantidad de Palabras: ");
+    this.nropalabras = Leer.datoInt();
+      for(int i=0;i<getNropalabras();i++){
+           palabra[i]=Leer.dato();
+        }
+    }
+    public void mostrar(){
+        System.out.println("Titulo: " + getTitulo());
+        for(int i=0; i<getNropalabras();i++){
+            System.out.print(palabra[i] + " ");
+            if(i%7==0){
+                System.out.println();
             }
         }
+        System.out.println();
+    }
+    public void mostrar(int x){
+        for(int i=0; i<getNropalabras();i++){
+            System.out.println("Palabra del Poema elegido: ");
+            System.out.println(palabra[x]);
+            break;
+        }
+    }
+    public void mostrar(int x, int y){
+        System.out.println("Rango de Palabras elegidas:");
+        for(int i=x;i<y;i++){
+                System.out.print(palabra[i] + " ");
+            if(i%4==0){
+                System.out.println();
+                }
+        }System.out.println();
+    }
+    
+    public static void main(String[] args){
+        Frase f1 = new Frase();
+        Frase f2 = new Frase();
+        f1.mostrar();
+        f1.mostrar(5);
+        f1.mostrar(7, 14);
+//        f2.leer();
+//        f2.mostrar();
+    }
         
 }
