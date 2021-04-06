2403353435c71816d26d932ae290be91d02a9f6a
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-05 20:49:26 -0400

    Terminando Biblioteca con codigo de ordenar por autor y por titulo de forma alfabetica

--- src/PracticaPoliformismo/Biblioteca.java
+++ src/PracticaPoliformismo/Biblioteca.java
@@ -18,10 +18,12 @@ public class Biblioteca {
     
     public Biblioteca(){
         this.nombre = "Biblioteca Central";
-        this.nrolibros = 3;
-        this.libro[0][0] = "12 Rules for Live"; this.libro[0][1] = "Jordan Peterson"; this.libro[0][2] = "Autoayuda"; 
+        this.nrolibros = 5;
+        this.libro[0][0] = "Maps of Meaning";   this.libro[0][1] = "Jordan Peterson"; this.libro[0][2] = "Autoayuda"; 
         this.libro[1][0] = "Clean Code";        this.libro[1][1] = "Robert Martin";   this.libro[1][2] = "Tecnologia"; 
         this.libro[2][0] = "Superintelligence"; this.libro[2][1] = "Nick Bostrom";    this.libro[2][2] = "Tecnologia"; 
+        this.libro[3][0] = "Zero to One";       this.libro[3][1] = "Peter Thiel";     this.libro[3][2] = "Negocios"; 
+        this.libro[4][0] = "Sapiens";           this.libro[4][1] = "Yuval Harari";    this.libro[4][2] = "Historia"; 
     }
 
     public Biblioteca(String nombre, int nrolibros) {
@@ -93,11 +95,59 @@ public class Biblioteca {
           }
         }
     }
-    
-    
+    public void ordenar(){
+        for(int i = 0; i<getNrolibros(); i++){
+            for(int j = 0; j<getNrolibros()&& i!=j;j++){
+                if(libro[i][0].compareToIgnoreCase(libro[j][0])<0){
+                    //titulo
+                    String aux = libro[i][0];
+                    libro[i][0]= libro[j][0];
+                    libro[j][0]= aux;
+                    //autor
+                    String aux1 = libro[i][1];
+                    libro[i][1] = libro[j][1];
+                    libro[j][1] = aux1;
+                    //categoria
+                    String aux2 = libro[i][2];
+                    libro[i][2] = libro[j][2];
+                    libro[j][2] = aux2;
+                }
+            }
+        }
+    }
+    public void ordenar(String x){
+        for(int i = 0; i<getNrolibros(); i++){
+            for(int j = 0; j<getNrolibros()&& i!=j;j++){
+                if(libro[i][1].compareToIgnoreCase(libro[j][1])<0){
+                    //autor
+                    String aux1 = libro[i][1];
+                    libro[i][1] = libro[j][1];
+                    libro[j][1] = aux1;
+                    //titulo
+                    String aux = libro[i][0];
+                    libro[i][0]= libro[j][0];
+                    libro[j][0]= aux;
+                    //categoria
+                    String aux2 = libro[i][2];
+                    libro[i][2] = libro[j][2];
+                    libro[j][2] = aux2;
+                }
+            }
+        }
+    }
     public static void main(String[] args){
         Biblioteca b1 = new Biblioteca();
         b1.mostrar();
         b1.mostrar("Jordan Peterson");
+        
+        System.out.println();
+        System.out.println("CATALOGO ORDENADO:");
+        b1.ordenar();
+        b1.mostrar();
+        
+        System.out.println();
+        System.out.println("CATALOGO ORDENADO POR AUTOR");
+        b1.ordenar("por Autor");
+        b1.mostrar();
     }
 }
