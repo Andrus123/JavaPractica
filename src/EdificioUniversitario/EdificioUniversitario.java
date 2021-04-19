14b44bf99ccd1cab660378daa671126f707e59d4
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-19 17:17:23 -0400

    clase EdificioUniversitario: al crear el array de objetos, defino la cantidad de Pisos que es la clase siguiente

--- src/EdificioUniversitario/EdificioUniversitario.java
+++ src/EdificioUniversitario/EdificioUniversitario.java
@@ -10,5 +10,17 @@ package EdificioUniversitario;
  * @author Andrés Aquin
  */
 public class EdificioUniversitario {
-    
+
+    private String nombre;
+    private int np;
+    private Piso[] pisos;
+
+    public EdificioUniversitario() {
+        this.nombre = "";
+        this.np = 10;
+        pisos = new Piso[np + 1];
+        for (int i = 1; i <= np; i++) {
+             pisos[i]=new Piso();
+        }
+    }
 }
