2e07a022214a73823903fa89f2edec52c21542af
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-19 17:27:43 -0400

    Clase principal, y creamos nuestro primer edificio universitario

---
+++ src/EdificioUniversitario/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EdificioUniversitario;
+
+/**
+ *
+ * @author Andrés Aquin
+ */
+public class Principal {
+    public static void main(String[] args){
+        EdificioUniversitario ed1 = new EdificioUniversitario();
+        ed1.leer();
+        ed1.mostrar();
+    }
+}
