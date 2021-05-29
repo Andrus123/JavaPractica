1e429f86899c92f02b103ca51a19f29973eda47b
Andrés K. Aquin <andruuus123@gmail.com>
2021-05-28 21:35:34 -0400

    Clase ArrayList

--- src/Clasesgenericas/Principal.java
+++ src/Clasesgenericas/Principal.java
@@ -27,7 +27,7 @@ public class Principal {
         //Clase Genérica ArrayList<E> 
         Usuarios u1 = new Usuarios("Carlos Aquin", "Ke@gmail.com", "Ke");
         Usuarios u2 = new Usuarios("Luis Fernando","luisfer@gmail.com","Nano");
-        Wallet w1 = new Wallet(700, "Bitcoin", 45000);
+        Wallet w1 = new Wallet(220, "Bitcoin", 40000);
         Wallet w2 = new Wallet(650, "Ethereum",15000);
        
         ArrayList<Usuarios> usuarios = new ArrayList<>();
